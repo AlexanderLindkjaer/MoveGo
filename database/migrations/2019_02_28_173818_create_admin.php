f53418f5538a2bad99afd043dee9a0cb7a943527
alexander_wecode <alexander@wecode.dk>
2019-02-28 18:42:11 +0100

    admins

---
+++ database/migrations/2019_02_28_173818_create_admin.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAdmin extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $p = \App\User::where('email', 'petertoftekaer@gmail.com')->first();
+        $a = \App\User::where('email', 'aksel-faxe@hotmail.com')->first();
+
+        if($p){
+            $p->is_admin = true;
+            $p->save();
+        }
+
+        if($a){
+            $a->is_admin = true;
+            $a->save();
+        }
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
